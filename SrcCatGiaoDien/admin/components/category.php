72941b582bc89df18785f275161f1e4503566310
Kimanh278 <kimanhdang278@gmail.com>
2025-11-25 17:16:54 +0700

    [HTML_Admin]quản lý danh mục sản phẩm

---
+++ SrcCatGiaoDien/admin/components/category.php
@@ -0,0 +1,33 @@
+<div class="card">
+    <div class="card-body">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Tên</th>
+                    <th scope="col">Gía</th>
+                    <th scope="col">Hình</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td colspan="2">Larry the </td>
+                    <td>@twitter</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
